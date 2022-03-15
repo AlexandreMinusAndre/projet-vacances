4d243c584437aa5e0d47a36f6544146da02b2ade
Teragi <72382774+Teragi@users.noreply.github.com>
2022-03-15 14:58:30 +0100

    commit

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelectorAll('nav button');
 console.log(menuBtn);
 menuBtn.forEach((el)=>{
     el.addEventListener('click', ()=>{
-        if (el === menuBtn[0]) window.location.href = 'home.html';
+        if (el === menuBtn[0]) window.location.href = 'index.html';
     });
 });
 user.addEventListener('click', ()=>{
