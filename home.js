dd9b27e09a21365ae9009bdad760eb5bf553b793
UneBaguette <thomasiger42@gmail.com>
2022-03-15 15:02:31 +0100

    This changes nothing, don't look

--- home.js
+++ home.js
@@ -1,15 +1,10 @@
 const user = document.querySelector('.user');
-const menuBtn = document.querySelectorAll('nav button');
-
-console.log(menuBtn)
-
-menuBtn.forEach(el => {
-    el.addEventListener('click', () => {
-        if (el === menuBtn[0]){
-            window.location.href='home.html';
-        }
-    })
-})
 
 user.addEventListener('click', () => {
+    console.log('ok')
+    if (!menu.classList.contains('active')){
+        menu.classList.add('active');
+    } else {
+        menu.classList.remove('active');
+    }
 })
--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const user = document.querySelector('.user');
+const menu = document.querySelector('.menu');
 const menuBtn = document.querySelectorAll('nav button');
-
-console.log(menuBtn)
 
 menuBtn.forEach(el => {
     el.addEventListener('click', () => {
         if (el === menuBtn[0]){
             window.location.href='index.html';
+        } else if (el === menuBtn[1]){
+            
         }
     })
-})
-
-user.addEventListener('click', () => {
 })
