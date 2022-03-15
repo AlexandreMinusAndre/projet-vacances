7f30c7baca53a4fc1accb21b9844d2320b0a887f
Teragi <72382774+Teragi@users.noreply.github.com>
2022-03-15 15:14:32 +0100

    commit

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -1,12 +1,15 @@
 const user = document.querySelector('.user');
+const menu = document.querySelector('.menu');
 const menuBtn = document.querySelectorAll('nav button');
-console.log(menuBtn);
+const connectionOverlay = document.querySelector('.connection-overlay');
 menuBtn.forEach((el)=>{
     el.addEventListener('click', ()=>{
         if (el === menuBtn[0]) window.location.href = 'index.html';
+        else if (el === menuBtn[1]) {
+            if (!connectionOverlay.classList.contains('activef')) connectionOverlay.classList.add('activef');
+            else connectionOverlay.classList.remove('activef');
+        }
     });
-});
-user.addEventListener('click', ()=>{
 });
 
 //# sourceMappingURL=index.672d4772.js.map
