0331a627b796f1798fcad1494c3cc27b90d5d3de
UneBaguette <thomasiger42@gmail.com>
2022-03-16 11:41:45 +0100

    more stuff

--- js/home.js
+++ js/home.js
@@ -2,10 +2,17 @@ const user = document.querySelector('.user');
 const menu = document.querySelector('.menu');
 
 user.addEventListener('click', () => {
-    console.log('ok')
-    if (!menu.classList.contains('active')){
-        menu.classList.add('active');
-    } else {
+    menu.classList.toggle('active');
+})
+
+document.onclick = function(e) {
+    if (!menu.contains(e.target) && !user.contains(e.target)){
         menu.classList.remove('active');
     }
-})
+    else if (menu.contains(e.target)){
+        menu.classList.add('active');
+    } 
+    else {
+        return;
+    }
+}
